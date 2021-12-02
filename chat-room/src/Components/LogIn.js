a802818b1b17720db6e96f2e405441fdf9ba2b81
yakov cohen <yakovc1431@gmail.com>
2021-12-02 15:01:29 +0200

    log in

--- chat-room/src/Components/LogIn.js
+++ chat-room/src/Components/LogIn.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function LogIn() {
-  return <div id="LogIn">log in</div>;
+  const navigate = useNavigate();
+
+  const loginInput = useRef(null);
+  const handleClick = () => {
+    console.log(loginInput.current.value);
+    navigate('/chat');
+  };
+  return (
+    <div id="LogIn">
+      <h1>Log In</h1>
+      <form className="forms">
+        <div className="container">
+          <span>Username</span>
+          <input
+            ref={loginInput}
+            type="text"
+            placeholder="Enter User name"
+            required
+          />
+          <button type="submit" onClick={handleClick}>
+            Login
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default LogIn;
