8f420e865c6a33702e83dda32246e24d17ec3627
yakov cohen <yakovc1431@gmail.com>
2021-12-02 16:59:39 +0200

    Revert "d"
    This reverts commit 4cfc35fbd1e3cc9c8501e7c9321302c90816a271.

--- server/index.js
+++ server/index.js
@@ -2,15 +2,6 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 8080;
 
-mongoose
-  .connect(mongo)
-  .then(result => {
-    console.log('connected to MongoDB');
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDB:', error.message);
-  });
-
 app.listen(PORT, () =>
   console.log(`app listening at http://localhost:${PORT}`)
 );
