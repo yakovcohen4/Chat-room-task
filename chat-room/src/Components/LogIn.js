9ab5e96b997b5f1fa0c0b3efd7a2c00e796c5074
yakov cohen <yakovc1431@gmail.com>
2021-12-02 22:04:50 +0200

    change log in and s-ccs

--- chat-room/src/Components/LogIn.js
+++ chat-room/src/Components/LogIn.js
@@ -1,5 +1,7 @@
 import React, { useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import '../styles/login.scss';
 
 function LogIn() {
   const navigate = useNavigate();
@@ -10,22 +12,37 @@ function LogIn() {
     navigate('/chat');
   };
   return (
-    <div id="LogIn">
-      <h1>Log In</h1>
-      <form className="forms">
-        <div className="container">
-          <span>Username</span>
-          <input
-            ref={loginInput}
-            type="text"
-            placeholder="Enter User name"
-            required
-          />
-          <button type="submit" onClick={handleClick}>
-            Login
-          </button>
+    <div className="body">
+      <div className="grad"></div>
+
+      <div className="header">
+        <div>
+          Chat<span>App</span>
         </div>
-      </form>
+        <div className="register">
+          <Link to={'/register'}>
+            <span className="register">Not a member? create account!</span>
+          </Link>
+        </div>
+      </div>
+
+      <div className="login">
+        <input
+          ref={loginInput}
+          type="text"
+          placeholder="username"
+          name="user"
+          required
+        />
+        <input
+          ref={loginInput}
+          type="password"
+          placeholder="Password"
+          name="password"
+          required
+        />
+        <input type="button" value="Login" onClick={handleClick}></input>
+      </div>
     </div>
   );
 }
