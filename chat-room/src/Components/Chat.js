5037f3856e0bd57b475a2786eee26ccae3aaca0e
yakov cohen <yakovc1431@gmail.com>
2021-12-02 15:01:39 +0200

    chat

---
+++ chat-room/src/Components/Chat.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Chat() {
+  return (
+    <div id="chat">
+      <h1>Chat</h1>
+      <form className="parent">
+        <div className="div1"> message </div>
+        <div className="div2"> input </div>
+        <div className="div3"> users </div>
+      </form>
+    </div>
+  );
+}
+
+export default Chat;
