a5f37ba08d13864d30c25cb209d65496ba4b09b5
yakov cohen <yakovc1431@gmail.com>
2021-12-02 15:01:57 +0200

    css

--- chat-room/src/Components/App.js
+++ chat-room/src/Components/App.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // components
-import Home from './Home';
 import LogIn from './LogIn';
+import Chat from './Chat';
 
 function App() {
+  const [state, setstate] = useState();
   return (
     <div>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<LogIn />} />
+          <Route path="/" element={<LogIn />} />
+          <Route path="/chat" element={<Chat />} />
         </Routes>
       </Router>
     </div>
