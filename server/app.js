322f34083ea53bc8f74699e250943eedc1e1ec58
yakov cohen <yakovc1431@gmail.com>
2021-12-02 20:15:20 +0200

    add user (router, error-handler) and cors

--- server/app.js
+++ server/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
-const app = express();
+require('dotenv').config();
 const morgan = require('morgan');
+const cors = require('cors');
 
+const app = express();
+
+//Routers
+const usersRouter = require('./routers/userRouter.js');
+
+//MiddleWares
+const { errorHandlerMiddleware } = require('./middlewares/errorHandler');
+
+/***** MIDDLEWARE *****/
+app.use(cors('*'));
 app.use(morgan('dev'));
 app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
@@ -9,5 +20,10 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('you in');
 });
+
+/***** ROUTERS *****/
+app.use('/users', usersRouter); // Register, Login, Logout
+
+app.use(errorHandlerMiddleware);
 
 module.exports = app;
