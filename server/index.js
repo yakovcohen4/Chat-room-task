ffaeee52fe3caa7b189a8aa7e42d7a38b61c457f
yakov cohen <yakovc1431@gmail.com>
2021-12-02 20:12:19 +0200

    connect to mongo

--- server/index.js
+++ server/index.js
@@ -1,7 +1,20 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const mongoose = require('mongoose');
 const app = require('./app');
 
 const PORT = process.env.PORT || 8080;
+const mongo = process.env.DATABASE;
 
-app.listen(PORT, () =>
-  console.log(`app listening at http://localhost:${PORT}`)
-);
+// connecting to database + listen
+mongoose
+  .connect(mongo)
+  .then(result => {
+    console.log('connected to MongoDB');
+    app.listen(PORT, () =>
+      console.log(`app listening at http://localhost:${PORT}`)
+    );
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message);
+  });
