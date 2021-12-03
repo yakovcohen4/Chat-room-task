ed57e8c10c5fae3240cce9e832280338cadd74c4
yakov cohen <yakovc1431@gmail.com>
2021-12-03 10:38:11 +0200

    add register function with err message

--- chat-room/src/Components/Register.js
+++ chat-room/src/Components/Register.js
@@ -1,9 +1,14 @@
-import React, { useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import '../styles/login-register.scss';
+import axios from 'axios';
+import BASEURL from './App';
+import { Error } from 'mongoose';
 
 function Register() {
+  const [error, setError] = useState();
+
   /***** REFS *****/
   const userNameInput = useRef('');
   const passwordInput = useRef('');
@@ -11,16 +16,36 @@ function Register() {
 
   const navigate = useNavigate();
 
-  const handleClick = () => {
-    console.log(userNameInput.current.value);
-    console.log(passwordInput.current.value);
-    console.log(emailInput.current.value);
-    navigate('/chat');
+  const register = async () => {
+    const userName = userNameInput.current.value;
+    const password = passwordInput.current.value;
+    const email = emailInput.current.value;
+    try {
+      // TODO ADD VALIDATION
+      if (!userName || !password || !email) {
+        throw new Error({ status: 400, message: 'Missing details' });
+      }
+      const response = await axios.post(
+        `http://localhost:8080/users/register`,
+        {
+          userName,
+          password: String(password),
+          email,
+        }
+      );
+      if (response.status === 200) {
+        navigate('/chat');
+      }
+      //   return response; //TODO- ADD SUCCESS MESSAGE
+      console.log(response.data);
+    } catch (error) {
+      console.log(error.message); //TODO- ADD ERROR MESSAGE
+      setError(error.message);
+    }
   };
   return (
     <div className="body">
       <div className="grad"></div>
-
       <div className="header">
         <div>
           Chat<span>App</span>
@@ -54,7 +79,8 @@ function Register() {
           name="password"
           required
         />
-        <input type="button" value="Sing Up" onClick={handleClick}></input>
+        <input type="button" value="Sing Up" onClick={register}></input>
+        <p>{error && <span className="err-mes">{error}</span>}</p>
       </div>
     </div>
   );
