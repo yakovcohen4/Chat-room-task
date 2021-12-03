12d7b45eb92f6fc1d3c4942ce70acb067b10b5ed
yakov cohen <yakovc1431@gmail.com>
2021-12-03 14:30:58 +0200

    chat component

--- chat-room/src/Components/Chat.js
+++ chat-room/src/Components/Chat.js
@@ -1,14 +1,35 @@
 import React from 'react';
+// import '../styles/chat.scss';
+import '../styles/chat.scss';
+
+import { Link } from 'react-router-dom';
+import MessageInput from './MessageInput';
+import MessageList from './MessageList';
+import MemberList from './MemberList';
 
 function Chat() {
+  const demoData = [
+    { author: 'yakov', message: 'Hello' },
+    { author: 'ori', message: 'hello to you' },
+    { author: 'rama', message: 'hello, I am rama' },
+    { author: 'ofir', message: 'hello, I am ofir' },
+    {
+      author: 'yakov',
+      message: "What's up dear friends",
+    },
+  ];
   return (
-    <div id="chat">
-      <h1>Chat</h1>
-      <form className="parent">
-        <div className="div1"> message </div>
-        <div className="div2"> input </div>
-        <div className="div3"> users </div>
-      </form>
+    <div className="chat-room">
+      <div className="messages-list">
+        <MessageList chatData={demoData} />
+      </div>
+      <div className="message-input">
+        <MessageInput />
+      </div>
+      <div className="members-list">
+        <h2>Members</h2>
+        <MemberList chatData={demoData} />
+      </div>
     </div>
   );
 }
