181de92ae079261df55acbaebd5ba5a480de115b
yakov cohen <yakovc1431@gmail.com>
2021-12-04 19:45:25 +0200

    front - not show me in members

--- chat-room/src/Components/MemberList.js
+++ chat-room/src/Components/MemberList.js
@@ -2,13 +2,17 @@ import React from 'react';
 import Member from './Member';
 
 function MemberList({ chatData }) {
+  const userName = sessionStorage.getItem('userName'); // get username - me or you ?
   const arrUsers = chatData.map(chatData => chatData.userName);
-  const uniqueArray = arrUsers.filter(function (item, pos) {
+  // console.log(arrUsers);
+  const uniqueArray = arrUsers.filter((item, pos) => {
     return arrUsers.indexOf(item) == pos;
   });
+  const arrWithoutUsername = uniqueArray.filter(user => user !== userName);
+  // console.log(arrWithoutUsername);
   return (
     <ul>
-      {uniqueArray.map((userName, index) => (
+      {arrWithoutUsername.map((userName, index) => (
         <Member key={index} name={userName} />
       ))}
     </ul>
