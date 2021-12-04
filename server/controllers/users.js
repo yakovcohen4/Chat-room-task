eabaeea8e83f47e04c72fe8cb6e85e7cf294b623
yakov cohen <yakovc1431@gmail.com>
2021-12-04 19:47:13 +0200

    back - add token in login

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,4 +1,5 @@
 const User = require('../modles/user');
+const jwt = require('jsonwebtoken');
 
 /***** FUNCTIONS ******/
 // Register by -userName, email and password
@@ -27,6 +28,7 @@ exports.register = async (req, res, next) => {
 // login by -userName and password
 exports.login = async (req, res, next) => {
   try {
+    const userForToken = req.body;
     const { userName, password } = req.body;
     const user = await User.find({ userName: userName, password: password });
 
@@ -34,7 +36,20 @@ exports.login = async (req, res, next) => {
       throw { status: 409, message: 'Username or password Incorrect' };
     }
 
-    res.send('login');
+    const tokens = {};
+    tokens.accessToken = jwt.sign(
+      userForToken,
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: '10s',
+      }
+    );
+    tokens.refreshToken = jwt.sign(
+      userForToken,
+      process.env.REFRESH_ACCESS_TOKEN_SECRET
+    );
+
+    res.send(tokens);
   } catch (err) {
     // console.log(err.message);
     next(err);
